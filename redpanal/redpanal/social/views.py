505ac3291804d287b5a836f0e9d2c755a0dd9e71
Santiago Piccinini <spiccinini@altermundi.net>
2014-01-27 22:41:34 -0300

    cherry-picked d-a-l add user stats menu

--- redpanal/redpanal/social/views.py
+++ redpanal/redpanal/social/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.template import RequestContext
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
+
 
 from actstream import actions, models
 from actstream.views import respond
@@ -31,22 +32,26 @@ def followers(request, content_type_id, object_id):
     Creates a listing of ``User``s that follow the actor defined by
     ``content_type_id``, ``object_id``.
     """
-    ctype = get_object_or_404(ContentType, pk=content_type_id)
-    actor = get_object_or_404(ctype.model_class(), pk=object_id)
-    return render_to_response('social/followers.html', {
-        'followers': models.followers(actor), 'actor': actor
-    }, context_instance=RequestContext(request))
-
+    if request.user.is_authenticated():
+        ctype = get_object_or_404(ContentType, pk=content_type_id)
+        actor = get_object_or_404(ctype.model_class(), pk=object_id)
+        return render_to_response('social/followers.html', {
+            'followers': models.followers(actor), 'actor': actor
+        }, context_instance=RequestContext(request))
+    else:
+        return redirect("/accounts/login/?next=/")
 
 def following(request, user_id):
     """
     Returns a list of actors that the user identified by ``user_id`` is following (eg who im following).
     """
-    user = get_object_or_404(User, pk=user_id)
-    return render_to_response('social/following.html', {
-        'following': models.following(user), 'user': user
-    }, context_instance=RequestContext(request))
-
+    if request.user.is_authenticated():
+        user = get_object_or_404(User, pk=user_id)
+        return render_to_response('social/following.html', {
+            'following': models.following(user), 'user': user
+        }, context_instance=RequestContext(request))
+    else:
+        return redirect("/accounts/login/?next=/")
 
 from forms import MessageForm
 from models import Message
