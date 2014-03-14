229992ab7394835dc09060dee6b13b1cd5a81c9e
Santiago Piccinini <spiccinini@altermundi.net>
2014-03-14 01:26:38 -0300

    added current git revision the footer

---
+++ redpanal/redpanal/__init__.py
@@ -0,0 +1,4 @@
+from utils.helpers import get_git_revision_short_hash
+
+VERSION = get_git_revision_short_hash()
+print VERSION
--- redpanal/redpanal/utils/helpers.py
+++ redpanal/redpanal/utils/helpers.py
@@ -2,3 +2,11 @@ import os
 
 def get_file_extension(filename):
     return os.path.splitext(filename)[1][1:].lower()
+
+def get_git_revision_short_hash():
+    import subprocess
+    try:
+        out = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).strip()
+    except:
+        out = ""
+    return out
