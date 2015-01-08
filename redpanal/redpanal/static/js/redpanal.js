1cbb4d17dfb2f84de356137db51b9e8a9f3a7b88
Santiago Piccinini <san@satellogic.com>
2015-01-08 16:54:20 -0300

    fixed reply msg

--- redpanal/redpanal/static/js/redpanal.js
+++ redpanal/redpanal/static/js/redpanal.js
@@ -4,10 +4,9 @@ $(function() {
   /* Comments */
   // Add reply action
   $("body").on("click", "button.comment-reply", function () {
-        console.log("asd")
         var username = $(this).data("username");
         $('html,body').animate({scrollTop: 0}, 500);
-        var input = $("#comment-form-small input#id_msg");
+        var input = $("#id_msg");
         input.focus();
         input.val("@"+username + " ");
     });
