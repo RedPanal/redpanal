edd3cf32b74c1d103c6defc93818b166f23982f0
Santiago Piccinini <san@satellogic.com>
2016-11-29 00:19:41 -0300

    GenericForeignKey updated import

--- redpanal/redpanal/social/models.py
+++ redpanal/redpanal/social/models.py
@@ -2,7 +2,7 @@ import re
 
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 from django.utils.safestring import mark_safe
@@ -34,7 +34,7 @@ class Message(models.Model):
                                            editable=False, related_name="mentioned_messages")
     content_type = models.ForeignKey(ContentType, null=True, editable=False)
     object_id = models.PositiveIntegerField(null=True, editable=False)
-    content_object = generic.GenericForeignKey('content_type', 'object_id')
+    content_object = GenericForeignKey('content_type', 'object_id')
     _msg_html_cache = models.TextField(editable=False, blank=True, null=True)
 
     def __unicode__(self):
