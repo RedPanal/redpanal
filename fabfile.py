573418023e4ecf6d69f7cb0a74019fc8e5ab2985
Santiago Piccinini <spiccinini@altermundi.net>
2020-05-12 01:42:52 -0300

    Upgrade fabric to v2.x

--- fabfile.py
+++ fabfile.py
@@ -8,21 +8,18 @@ defaults if you are happy with the default layout.
 """
 
 import posixpath
+import datetime
 
-from fabric.api import run, local, abort, env, put, settings, cd, task, sudo
-from fabric.decorators import runs_once
-from fabric.contrib.files import exists
-from fabric.context_managers import cd, lcd, settings, hide
+from fabric import Connection
 
 # CHANGEME:
 
 USER = 'sanpiccinini'
-HOST = 'panal.codigosur.com'
+HOST = 'redpanal.org'
 APP_NAME = 'redpanal'
 REPOSITORY = "ssh://git@github.com:RedPanal/redpanal.git"
 
-# Host and login username:
-env.hosts = ['%s@%s' % (USER, HOST)]
+PORT = 22232
 
 # Directory where everything to do with this app will be stored on the server.
 DJANGO_APP_ROOT = '/var/www/%s' % APP_NAME
@@ -49,153 +46,176 @@ VENV_DIR = posixpath.join(DJANGO_APP_ROOT, VENV_SUBDIR)
 
 WSGI_MODULE = '%s.wsgi' % APP_NAME
 
+env = {}
 
-def virtualenv(venv_dir):
-    """
-    Context manager that establishes a virtualenv to use.
-    """
-    return settings(venv=venv_dir)
+def dir_exists(c, path):
+    return c.run(f"test -d {path}").ok == True
 
+def test(c):
+    c.run("uname -a")
 
-def run_venv(command, **kwargs):
+def run_venv(c, command, **kwargs):
     """
     Runs a command in a virtualenv (which has been specified using
     the virtualenv context manager
     """
-    run("source %s/bin/activate" % env.venv + " && " + command, **kwargs)
+    c.run("source %s/bin/activate" % VENV_DIR + " && " + command, **kwargs)
 
 
-def install_dependencies():
-    ensure_virtualenv()
-    with virtualenv(VENV_DIR):
-        with cd(SRC_DIR):
-            run_venv("pip install -r requirements.txt")
+def install_dependencies(c):
+    ensure_virtualenv(c)
+    run_venv(c, f"cd {SRC_DIR} && pip install -r requirements.txt")
 
-
-def ensure_virtualenv():
-    if exists(VENV_DIR):
+def ensure_virtualenv(c):
+    if dir_exists(c, VENV_DIR):
         return
 
-    with cd(DJANGO_APP_ROOT):
-        run("virtualenv --no-site-packages --python=%s %s" %
-            (PYTHON_BIN, VENV_SUBDIR))
-        run("echo %s > %s/lib/%s/site-packages/projectsource.pth" %
-            (SRC_DIR, VENV_SUBDIR, PYTHON_BIN))
+    c.run(f"cd {DJANGO_APP_ROOT} && virtualenv --no-site-packages --python={PYTHON_BIN} {VENV_SUBDIR}")
+    c.run(f"cd {DJANGO_APP_ROOT} && echo {SRC_DIR} > {VENV_SUBDIR}/lib/{PYTHON_BIN}/site-packages/projectsource.pth")
 
 
-def ensure_src_dir():
-    if not exists(SRC_DIR):
-        run("mkdir -p %s" % SRC_DIR)
-    with cd(SRC_DIR):
-        if not exists(posixpath.join(SRC_DIR, '.git')):
-            run("git init")
+def ensure_src_dir(c):
+    if not dir_exists(c, SRC_DIR):
+        c.run(f"mkdir -p {SRC_DIR}")
+        
+    if not dir_exists(c, posixpath.join(SRC_DIR, '.git')):
+        c.run(f"cd {SRC_DIR} && git init")
 
-
-@task
-def push_rev(rev):
-    """
-    Use the specified revision for deployment, instead of the current revision.
-    """
-    env.push_rev = rev
-
-
-def push_sources():
+def push_sources(c):
     """
     Push source code to server.
     """
-    ensure_src_dir()
+    ensure_src_dir(c)
     push_rev = getattr(env, 'push_rev', None)
     if push_rev is None:
-        push_rev = local("git rev-parse HEAD", capture=True).strip()
+        push_rev = c.local("git rev-parse HEAD").stdout.strip()
 
-    local("git push ssh://%(user)s@%(host)s/%(path)s || true" %
-          dict(host=env.host,
-               user=env.user,
-               path=SRC_DIR,
-               ))
-    with cd(SRC_DIR):
-        run("git checkout %s" % push_rev)
+    c.local("git push ssh://%(user)s@%(host)s:%(port)s/%(path)s || true" %
+            dict(host=HOST,
+                 user=USER,
+                 port=PORT,
+                 path=SRC_DIR,
+                 ))
+    c.run(f"cd {SRC_DIR} && git checkout %s" % push_rev)
 
 
-@task
-def webserver_stop():
+
+def webserver_stop(c):
     """
     Stop the webserver that is running the Django instance
     """
-    sudo("/etc/init.d/uwsgi stop")
+    c.sudo("systemctl stop uwsgi.service")
 
-@task
-def webserver_start():
+
+def webserver_start(c):
     """
     Starts the webserver that is running the Django instance
     """
-    sudo("/etc/init.d/uwsgi start")
+    c.sudo("systemctl start uwsgi.service")
 
 
-
-@task
-def webserver_restart():
+def webserver_restart(c):
     """
     Restarts the webserver that is running the Django instance
     """
     try:
-        sudo("/etc/init.d/uwsgi restart")
+        c.sudo("systemctl restart uwsgi.service")
     except:
-        webserver_start()
+        webserver_start(c)
 
 
-def build_static():
-    if not exists(STATIC_ROOT):
-        run("mkdir -p %s" % STATIC_ROOT)
-    with virtualenv(VENV_DIR):
-        with cd(MANAGEPY_SUBDIR):
-            run_venv("%s manage.py collectstatic --settings=redpanal.production_settings -v 0 --noinput --clear --link" % PYTHON_BIN)
+def build_static(c):
+    
+    if not dir_exists(c, STATIC_ROOT):
+        c.run("mkdir -p %s" % STATIC_ROOT)
 
-    run("chmod -R ugo+r %s" % STATIC_ROOT)
+    run_venv(c, f"cd {MANAGEPY_SUBDIR} && {PYTHON_BIN} manage.py collectstatic --settings=redpanal.production_settings -v 0 --noinput --clear --link")
+
+    c.run(f"chmod -R ugo+r {STATIC_ROOT}")
+
+def clear_sessions(c):
+    run_venv(c, f"cd {MANAGEPY_SUBDIR} && {PYTHON_BIN} manage.py clearsessions --settings=redpanal.production_settings")    
 
 
-@task
-def first_deployment_mode():
+def first_deployment_mode(c):
     """
     Use before first deployment to switch on fake south migrations.
     """
     env.initial_deploy = True
 
 
-def update_database():
-    with virtualenv(VENV_DIR):
-        with cd(MANAGEPY_SUBDIR):
-            managepy = "%s manage.py " % PYTHON_BIN
-            if getattr(env, 'initial_deploy', False):
-                run_venv(managepy + "migrate --fake-initial --noinput --settings=redpanal.production_settings")
-            else:
-                run_venv(managepy + "migrate --noinput --settings=redpanal.production_settings")
+def update_database(c):
+    managepy = f"cd {MANAGEPY_SUBDIR} && {PYTHON_BIN} manage.py "
+    if getattr(env, 'initial_deploy', False):
+        run_venv(c, managepy + "migrate --fake-initial --noinput --settings=redpanal.production_settings")
+    else:
+        run_venv(c, managepy + "migrate --noinput --settings=redpanal.production_settings")
 
-@task
-def backup_database():
-    run("cp /var/www/redpanal/db/db.sqlite3 ~/redpanal.db.sqlite3_`date +%d-%m-%y`")
 
-@task
-def rebuild_index():
+def backup_database(c):
+    datestr = datetime.datetime.now().isoformat()
+    c.run("cp /var/www/redpanal/db/db.sqlite3 ~/redpanal.db.sqlite3_%s" % datestr)
+    c.run("gzip ~/redpanal.db.sqlite3_%s" % datestr)
+
+
+def rebuild_index(c):
     managepy = "%s manage.py " % PYTHON_BIN
-    with virtualenv(VENV_DIR):
-        with cd(MANAGEPY_SUBDIR):
-            run_venv(managepy + "rebuild_index --noinput --settings=redpanal.production_settings")
-    sudo("chown -R www-data /var/www/redpanal/whoosh_index")
+    run_venv(c, f'cd {MANAGEPY_SUBDIR} && ' + managepy + "rebuild_index --noinput --settings=redpanal.production_settings")
+    c.sudo("chown www-data /var/www/redpanal/whoosh_index/*")
 
-@task
-def deploy():
+def deploy(c):
     """
     Deploy project.
     """
-    with settings(warn_only=True):
-        webserver_stop()
-    push_sources()
-    install_dependencies()
-    backup_database()
-    update_database()
-    #rebuild_index()
-    build_static()
+    webserver_stop(c)
+    push_sources(c)
+    install_dependencies(c)
+    clear_sessions(c)
+    backup_database(c)
+    update_database(c)
+    rebuild_index(c)
+    build_static(c)
+    webserver_start(c)
 
-    webserver_start()
 
+"""
+anonymous data
+sqlite> UPDATE "auth_user" SET "email" = 'foo@bar.com';
+sqlite> UPDATE "auth_user" SET "password" = '';
+sqlite> UPDATE "auth_user" SET "first_name" = '';
+sqlite> UPDATE "auth_user" SET "last_name" = '';
+sqlite> DROP table account_emailaddress;
+"""
+
+TASKS = {
+    'test': test,
+    'ensure_src_dir': ensure_src_dir,
+    'push_sources': push_sources,
+    'webserver_start': webserver_start,
+    'webserver_stop': webserver_stop,
+    'webserver_restart': webserver_stop,
+    'rebuild_index': rebuild_index,
+    'install_dependencies': install_dependencies,
+    'clear_sessions': clear_sessions,
+    'backup_database': backup_database,
+    'update_database': update_database,
+    'build_static': build_static,
+    'deploy': deploy,
+}
+
+if __name__ == "__main__":
+    from functools import partial
+    import argparse
+    parser = argparse.ArgumentParser(description='Redpanal deploys')
+
+    parser.add_argument('cmd', choices=TASKS.keys())
+    parser.add_argument('--revision', help="Use a specific git revision")
+    args = parser.parse_args()
+
+    task = TASKS[args.cmd]
+    c = Connection(f'{USER}@{HOST}', port=PORT)
+    c.run = partial(c.run, echo=True)
+    c.sudo = partial(c.sudo, echo=True)
+    task(c)
+    
+    
