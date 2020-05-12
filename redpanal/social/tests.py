3f3d6590631ecdbe7272eaa89a96595fd64d8e64
Santiago Piccinini <spiccinini@altermundi.net>
2020-05-11 21:41:42 -0300

    Tags are added without order

--- redpanal/social/tests.py
+++ redpanal/social/tests.py
@@ -31,7 +31,7 @@ class MessageTest(TestCase):
     def test_tags(self):
         msg = "@owner here this #radioGaGa #Meith we are"
         m = Message.objects.create(user=self.user, msg=msg)
-        self.assertEqual(list(map(lambda t: t.name, m.tags.all())), ["radioGaGa", "Meith"])
+        self.assertEqual(set(m.tags.values_list('name', flat=True)), {"radioGaGa", "Meith"})
 
     def test_tag_html(self):
         msg = "@owner here this #radioGaGa #Meith we are"
