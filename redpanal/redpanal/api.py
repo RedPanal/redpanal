c90bb690ef133033bb3a4d78da90a9856787e237
Santiago Piccinini <spiccinini@altermundi.net>
2020-06-06 20:18:03 -0300

    api: add a filterable audio list

--- redpanal/redpanal/api.py
+++ redpanal/redpanal/api.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User, Group
+from django.conf.urls import include, url
 from audio.models import Audio
-from rest_framework import routers, serializers, viewsets
+from rest_framework import routers, serializers, viewsets, generics
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from taggit_serializer.serializers import (TagListSerializerField,
@@ -12,7 +13,7 @@ class AudioSerializer(TaggitSerializer, serializers.ModelSerializer):
     class Meta:
         model = Audio
         fields = (
-            'id', 'slug', 'name', 'audio', 'created_at', 'license',
+            'id', 'slug', 'name', 'audio', 'user', 'created_at', 'license',
             'description', 'totalframes', 'samplerate',
             'use_type', 'genre', 'instrument', 'tags',
             'position_lat', 'position_long',
@@ -26,21 +27,44 @@ class AudioViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-    @action(detail=False)
-    def positioned(self, request):
-        audios = Audio.objects.exclude(position_long__isnull=True,
-                                       position_lat__isnull=True).order_by('-created_at')
 
-        page = self.paginate_queryset(audios)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
+class AudioList(generics.ListAPIView):
+    """
+    List audios. Query parameters can be used to filter the list.
+    Eg: audio/list/?user=redpanal&genre=rock&tag=awesome
 
-        serializer = self.get_serializer(audios, many=True)
-        return Response(serializer.data)
+    Query parameters: [user, genre, instrument, use_type, tag]
+    Note that tag can be provided multiple times no narrow more the list (Eg /?tag=foo&tag=bar)
+    """
+    serializer_class = AudioSerializer
 
+    def get_queryset(self):
+        queryset = Audio.objects.all()
+
+        filter_data = {}
+        username = self.request.query_params.get('user', None)
+        if username is not None:
+            filter_data['user__username'] = username
+
+        for filter_param in ['genre', 'instrument', 'use_type']:
+            param = self.request.query_params.get(filter_param, None)
+            if param is not None:
+                filter_data[filter_param] = param
+
+        queryset = queryset.filter(**filter_data)
+
+        tags = self.request.query_params.getlist('tag')
+        for tag in tags:
+            queryset = queryset.filter(tags__slug=tag)
+
+        if self.request.query_params.getlist('positioned', None):
+            queryset = queryset.exclude(position_long__isnull=True, position_lat__isnull=True)
+
+        return queryset.order_by('-created_at')
 
 api_router = routers.DefaultRouter()
 api_router.register(r'audio', AudioViewSet, basename='audio-api')
 
-api_urls = api_router.urls
+api_urls = [
+    url('^audio/list/$', AudioList.as_view()),
+] + api_router.urls
