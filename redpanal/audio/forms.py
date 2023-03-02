0cd70af05e112f9888e13248a82f632bc8838293
Alitux <alitux@disroot.org>
2023-03-02 19:22:35 -0300

    Solve Multiple Upload

--- redpanal/audio/forms.py
+++ redpanal/audio/forms.py
@@ -14,11 +14,44 @@ from multiupload.fields import MultiFileField
 
 AUDIO_EXTENSIONS = ["mp3", "ogg", "oga", "flac", "wav"]
 
-class AudioForm(forms.ModelForm):
 
+class AudioEditForm(forms.ModelForm):
+    ## Edit Audio Form
 
-    # tags = TagField(required=False)
-    # project = forms.ModelChoiceField(Project.objects.all(), required=False)
+    tags = TagField(required=False)
+    project = forms.ModelChoiceField(Project.objects.all(), required=False)
+    audio = forms.FileField(label=_("Audio"), help_text=_("Allowed extensions: %s") % ", ".join(AUDIO_EXTENSIONS))
+
+    helper = FormHelper()
+    helper.form_class = 'form-horizontal'
+    helper.add_input(Submit('submit', 'Submit'))
+    class Meta:
+        model = Audio
+        widgets = {
+            'license': forms.RadioSelect,
+        }
+        fields = '__all__'
+
+    def __init__(self, data=None, *args, **kwargs):
+        user = kwargs.pop('user')
+        super(AudioEditForm, self).__init__(data, *args, **kwargs)
+        self.instance.user = user
+        initial_project = self.instance.project_set.all()[0] if self.instance.pk and \
+                         self.instance.project_set.all() else None
+        self.fields['project'] = forms.ModelChoiceField(queryset=Project.objects.filter(user=user),
+                                                        initial=initial_project, required=False)
+
+    def clean_audio(self):
+        f = self.cleaned_data.get('audio', False)
+        if not f:
+            raise ValidationError(_("Couldn't read uploaded file"))
+        if not get_file_extension(f.name) in AUDIO_EXTENSIONS:
+            raise ValidationError(_("Invalid audio file extension"))
+        return f
+
+class AudioUploadForm(forms.ModelForm):
+    #Upload Form
+
     audio = MultiFileField(
         label=_("Audio"), 
         help_text=_("Allowed extensions: %s") % ", ".join(AUDIO_EXTENSIONS))
@@ -36,9 +69,9 @@ class AudioForm(forms.ModelForm):
 
     def __init__(self, data=None, *args, **kwargs):
         user = kwargs.pop('user')
-        super(AudioForm, self).__init__(data, *args, **kwargs)
+        super(AudioUploadForm, self).__init__(data, *args, **kwargs)
         self.instance.user = user
-        
+
     def clean_audio(self):
         files = self.cleaned_data.get('audio', False)
         for f in files:
--- redpanal/audio/views.py
+++ redpanal/audio/views.py
@@ -4,23 +4,51 @@ import re
 import string
 
 from django.contrib.auth.decorators import login_required
-
+from django.utils.translation import ugettext_lazy as _
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, UpdateView, DetailView, CreateView, DeleteView, FormView
 
 from .models import Audio
 from project.models import Project
-from .forms import AudioForm
+from .forms import AudioEditForm, AudioUploadForm
 from redpanal.utils.views import LoginRequiredMixin, UserRequiredMixin
 from django.forms import formset_factory
 
 @login_required
 def audio_create_update(request, slug=None):
-    ## Upload files
+    ##Edit Audio
+    
     instance = get_object_or_404(Audio, slug=slug) if slug else None
+    title = _("Edit Audio")
     if request.method == "POST":
-        form = AudioForm(request.POST, request.FILES, 
+        form = AudioEditForm(request.POST, request.FILES, user=request.user,
+                         instance=instance)
+        if form.is_valid():
+            project = form.cleaned_data.get("project")
+            if project:
+                project = get_object_or_404(Project, pk=project.pk)
+            audio = form.save()
+            if project:
+                project.audios.add(audio)
+            return redirect("audio-detail", slug=audio.slug)
+    else:
+        form = AudioEditForm(user=request.user, instance=instance)
+
+    return render(request, "audio/audio_form.html", {
+        'form': form,
+        'object': instance,
+        'title': title
+    })
+
+@login_required
+def audio_upload(request, slug=None):
+    ## Upload files
+
+    instance = get_object_or_404(Audio, slug=slug) if slug else None
+    title = _("Upload Audio") 
+    if request.method == "POST":
+        form = AudioUploadForm(request.POST, request.FILES, 
                         user=request.user, 
                         instance=instance
                          )
@@ -30,11 +58,12 @@ def audio_create_update(request, slug=None):
             return redirect("index")
 
     else:
-        form = AudioForm(user=request.user, instance=instance)
+        form = AudioUploadForm(user=request.user, instance=instance)
 
     return render(request, "audio/audio_form.html", {
         'form': form,
         'object': instance,
+        'title': title
     })
 
 class AudioDetailView(DetailView):
