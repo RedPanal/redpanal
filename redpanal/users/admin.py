3126692295e7e87048726ff34d7484855aa6e80d
Alitux <alitux@disroot.org>
2023-03-03 11:06:01 -0300

    Add comments and remove Audio in Admin Site

--- redpanal/users/admin.py
+++ redpanal/users/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from .models import DefaultFollowedUser
-from audio.models import Audio #BORRAME
+# from audio.models import Audio #BORRAME
 
 class DefaultFollowedUserAdmin(admin.ModelAdmin):
     list_display = ('user', )
     
 admin.site.register(DefaultFollowedUser, DefaultFollowedUserAdmin)
-admin.site.register(Audio) #BORRAME
+# admin.site.register(Audio)
